670a16cfbffec25410aa0de45252c8bb416f687f
Artem Shchurev <artemshchurev@gmail.com>
2024-10-25 08:50:41 +1000

    Added sip connection start

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:sip_ua/sip_ua.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> implements SipUaHelperListener {
+  final SIPUAHelper _sipuaHelper = SIPUAHelper();
+
+
+  void login() async {
+    final UaSettings uaSettings = UaSettings()
+      ..webSocketUrl = 'wss://sip.otens.ru:8089/ws'
+      ..password = '1909video2024'
+      ..userAgent = '113'
+      ..transportType = TransportType.WS
+      ..webSocketSettings.allowBadCertificate = true
+      ..uri = '113@sip.otens.ru'
+      ..displayName = '113';
+    await _sipuaHelper.start(uaSettings);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    login();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,55 +42,52 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('title'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'here',
+              ),
+            ],
+          ),
+        ),
+      )
     );
+  }
+
+  @override
+  void callStateChanged(Call call, CallState state) {
+    // TODO: implement callStateChanged
+  }
+
+  @override
+  void onNewMessage(SIPMessageRequest msg) {
+    // TODO: implement onNewMessage
+  }
+
+  @override
+  void onNewNotify(Notify ntf) {
+    // TODO: implement onNewNotify
+  }
+
+  @override
+  void onNewReinvite(ReInvite event) {
+    // TODO: implement onNewReinvite
+  }
+
+  @override
+  void registrationStateChanged(RegistrationState state) {
+    // TODO: implement registrationStateChanged
+  }
+
+  @override
+  void transportStateChanged(TransportState state) {
+    // TODO: implement transportStateChanged
   }
 }
